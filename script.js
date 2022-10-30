4da78ee0983d2037d952e081dfc2f9a35d2f04e3
Srilakshmi R <114687168+srilakshmiranganath@users.noreply.github.com>
2022-10-30 20:28:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const num1 = Math.ceil(Math.random()*10)
+const num2 = Math.ceil(Math.random()*10)
+
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+let score = JSON.parse(localStorage.getItem("score"));
+const scoreEl = document.getElementById("score");
+
+if(!score){
+    score = 0;
+}
+
+const questionEl = document.getElementById("question");
+questionEl.innerText = `What is ${num1} multiplied by ${num2}?`
+
+const correctAns = num1 * num2
+
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value
+    
+    if(userAns == correctAns){
+        score++
+        updateLocalStorage()
+    }
+    else{
+        score--
+        updateLocalStorage()
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
+
+scoreEl.innerText = `SCORE : ${score}`
